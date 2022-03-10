7b93b6a830f17bfdcaf98c1487cd2c4f69d52a0d
Andrew-Smirnoff <asmirnoff@my.bcit.ca>
2022-03-09 17:57:14 -0800

    Created player and enemy objects

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string unitName;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public int damage;
+
+    public int maxHP;
+    public int currentHP;
+
+	public bool TakeDamage(int dmg)
+	{
+		currentHP -= dmg;
+
+		if (currentHP <= 0)
+			return true;
+		else
+			return false;
+	}
 }
