5f9989c81fc8541edef66687184508d0b8e9d317
Andrew-Smirnoff <asmirnoff@my.bcit.ca>
2022-03-09 19:27:40 -0800

    Added setup state for combat encounter

---
+++ Assets/BattleSystem.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class BattleSystem : MonoBehaviour
+{
+
+	public GameObject playerPrefab;
+	public GameObject enemyPrefab;
+
+	public Transform playerBattleStation;
+	public Transform enemyBattleStation;
+
+	Unit playerUnit;
+	Unit enemyUnit;
+
+	public BattleState state;
+
+	void Start()
+    {
+		state = BattleState.START;
+		StartCoroutine(SetupBattle());
+	}
+
+	IEnumerator SetupBattle()
+	{
+		GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
+		playerUnit = playerGO.GetComponent<Unit>();
+
+		GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
+		enemyUnit = enemyGO.GetComponent<Unit>();
+
+		yield return new WaitForSeconds(2f);
+
+		state = BattleState.PLAYERTURN;
+	}
+}
