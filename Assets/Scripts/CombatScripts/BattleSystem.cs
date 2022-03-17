1b0bdcc4803d92db23f2d7640cceae3cc7e8bff6
Andrew-Smirnoff <asmirnoff@my.bcit.ca>
2022-03-17 02:36:29 -0700

    Updated battle interface

--- Assets/Scripts/CombatScripts/BattleSystem.cs
+++ Assets/Scripts/CombatScripts/BattleSystem.cs
@@ -9,39 +9,55 @@ public class BattleSystem : MonoBehaviour
 {
 
 	public GameObject playerPrefab;
-	public GameObject enemyPrefab;
+	public GameObject enemy1;
+	public GameObject enemy2;
+	public GameObject enemy3;
+	public GameObject enemy4;
+
 
 	public Transform playerBattleStation;
-	public Transform enemyBattleStation;
+	public Transform enemyBattleStation1;
+	public Transform enemyBattleStation2;
+	public Transform enemyBattleStation3;
+	public Transform enemyBattleStation4;
 
 	Unit playerUnit;
 	Unit enemyUnit;
 
 	public Text InfoText;
 
-	public BattleHud enemyHud;
-	public BattleHud playerHud;
-
 	public BattleState state;
 
 	void Start()
     {
+
 		state = BattleState.START;
 		StartCoroutine(SetupBattle());
 	}
 
 	IEnumerator SetupBattle()
 	{
-		GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
-		playerUnit = playerGO.GetComponent<Unit>();
 
-		GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
-		enemyUnit = enemyGO.GetComponent<Unit>();
+		GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
+
+		if (enemy1 != null){
+			GameObject enemyGO1 = Instantiate(enemy1, enemyBattleStation1);
+			enemyUnit = enemyGO1.GetComponent<Unit>();
+		}
+		if (enemy2 != null){
+			GameObject enemyGO2 = Instantiate(enemy2, enemyBattleStation2);
+		}
+		if (enemy3 != null){
+			GameObject enemyGO3 = Instantiate(enemy3, enemyBattleStation3);
+		}
+		if (enemy4 != null){
+			GameObject enemyGO4 = Instantiate(enemy4, enemyBattleStation4);
+		}
 
 		InfoText.text = "Test Battle";
 
-		enemyHud.SetupHud(enemyUnit);
-		playerHud.SetupHud(playerUnit);
+		//enemyHud.SetupHud(enemyUnit);
+		//playerHud.SetupHud(playerUnit);
 
 		yield return new WaitForSeconds(2f);
 
@@ -52,7 +68,7 @@ public class BattleSystem : MonoBehaviour
 	{
 		bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
 
-		enemyHud.UpdateHud(enemyUnit);
+		//enemyHud.UpdateHud(enemyUnit);
 		InfoText.text = "The player hits the enemy!";
 
 		yield return new WaitForSeconds(2f);
@@ -77,7 +93,7 @@ public class BattleSystem : MonoBehaviour
 		yield return new WaitForSeconds(1f);
 
 		bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
-		playerHud.UpdateHud(playerUnit);
+		//playerHud.UpdateHud(playerUnit);
 
 		yield return new WaitForSeconds(1f);
 
@@ -94,7 +110,7 @@ public class BattleSystem : MonoBehaviour
 
 	}
 
-	void EndBattle()
+	void EndBattle() 
 	{
 		if (state == BattleState.WON)
 		{
